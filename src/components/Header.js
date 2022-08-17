43c19146ea9784ee4083d5482ac7ec0a68b9f06e
Thomas Denoo <denoo.thomas@gmail.com>
2022-08-17 17:23:45 +0200

    cart styling changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,8 @@ export default function Header(){
         <div className="header-cont">
             <Link to="/"><h1 className="header-title">Pic Some</h1></Link>
             <Link to="cart">
-                <div className="cart-cont">
-                    <img className="cart-img" src={cartImg} alt="shopping cart"></img>
+                <div className="cart-header-cont">
+                    <img className="cart-header-img" src={cartImg} alt="shopping cart"></img>
                     <p className="cart-counter">{cart.length}</p>
                 </div>    
             </Link>
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react"
 
 import "./styling.css"
+import binImg from "../../img/delete-bin.svg"
 import { Context } from "../../context/Context"
 
 export default function Cart(){
@@ -10,15 +11,20 @@ export default function Cart(){
     console.log("allPhotos => ", allPhotos)
     console.log("cart => ", cart)
 
+    const price = 5.95
+    const totalPrice = cart.length*price
+
     const cartElements = 
         allPhotos.map(photoObj => {
             return cart.map(itemID => {
                 if(photoObj.id === itemID){
                     return (
                         <div className="cart-line" key={itemID}>
-                            <img alt="cart option" src={photoObj.url} width="50px" height="50px"/>
-                            <div className="cart-options-cont">
-                                <p>itemID - {itemID}</p>
+                            <img src={photoObj.url} className="cart-img" alt="cart option"/>
+                            <div className="cart-black-line"></div>
+                            <div className="cart-option-cont">
+                                <img src={binImg} className="cart-option-icon" alt="cart option"></img>
+                                <p className="cart-price">${price}</p>
                             </div>
                         </div>
                     )
@@ -27,9 +33,12 @@ export default function Cart(){
         })
 
     return(
-        <div className="cart-cont">
+        <React.Fragment>
             <h1 className="cart-title">Check out</h1>
-            {cartElements}
-        </div>
+            <div className="cart-cont">
+                {cartElements}
+                <p className="cart-line cart-totalprice">Total price: ${totalPrice}</p>
+            </div>
+        </React.Fragment>
     )
 }
